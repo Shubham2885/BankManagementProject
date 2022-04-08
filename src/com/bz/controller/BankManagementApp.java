d72d9d94922b290eba3908ddf691a2047ec0a4ae
Shubham2885 <shubham.chavan689977@gmail.com>
2022-04-08 20:45:34 +0530

    project structuere

---
+++ src/com/bz/controller/BankManagementApp.java
@@ -0,0 +1,14 @@
+package com.bz.controller;
+
+public class BankManagementApp {
+
+	public static void main(String[] args) {
+		
+	}
+}
+
+//Customer( id, firstName, middleName, lastName, mobileNo, username, password, email, address _id)
+//Address(street, plotNo, town, taluka, district, state, pincode)
+//Account(accountNo, ifscNo, branchCode, accountType, balance, branchName, customer_id)
+//BankMangementDashboard
+//openAccount, debit, credit, banlanceEnquiry, tranferfund
