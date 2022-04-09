4fbec131a886c96b0b748551cbe766987c3a5a0b
Shubham2885 <shubham.chavan689977@gmail.com>
2022-04-09 19:27:53 +0530

    BMS-1 Create a Entity class with below details

--- src/com/bz/model/Customer.java
+++ src/com/bz/model/Customer.java
@@ -2,4 +2,74 @@ package com.bz.model;
 
 public class Customer {
 
+	private int id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String mobileNo; 
+	private String username;
+	private String password;
+	private String email;
+	private int addressId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName="
+				+ lastName + ", mobileNo=" + mobileNo + ", username=" + username + ", password=" + password + ", email="
+				+ email + ", addressId=" + addressId + "]";
+	}
+	
 }
